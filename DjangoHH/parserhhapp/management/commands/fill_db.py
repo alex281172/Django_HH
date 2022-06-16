43cfc2071858e16681bce32576636b5b189b8ba1
Aleksey <alex281172@mail.ru>
2022-06-16 08:27:12 +0400

    CBV

--- DjangoHH/parserhhapp/management/commands/fill_db.py
+++ DjangoHH/parserhhapp/management/commands/fill_db.py
@@ -1,8 +1,11 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
+import os
 from parserhhapp.models import Cities, Skills
 
 class Command(BaseCommand):
-
+    path = os.path.join(settings.Base_DIR, 'parserhhapp', 'my.json')
     def handle(self, *args, **options):
+
         print('Hello')
 
